2fdc6033fe30bc7bff197b67e45abc523a450427
Jason Wei <jason.william.wei@gmail.com>
2025-08-28 15:40:55 +0000

    Removed Terminal Engine from rllm/engine/rollout/__init__.py

--- rllm/engine/rollout/__init__.py
+++ rllm/engine/rollout/__init__.py
@@ -1,10 +1,8 @@
 from .openai_engine import OpenAIEngine
 from .rollout_engine import ModelOutput, RolloutEngine
-from .terminal_litellm_engine import TerminalLiteLLMEngine
 
 __all__ = [
     "ModelOutput",
     "RolloutEngine",
     "OpenAIEngine",
-    "TerminalLiteLLMEngine",
 ]
