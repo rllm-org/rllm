d25b38d1df7ed3f14da0d93036bec29b5d0bea94
1stprinciple <ztyharbin@gmail.com>
2025-10-22 06:26:32 +0200

    +

--- examples/fireworks_math/train_fireworks_math.sh
+++ examples/fireworks_math/train_fireworks_math.sh
@@ -28,7 +28,7 @@ python3 -m examples.fireworks_math.train_fireworks_math \
     actor_rollout_ref.hybrid_engine=True \
     actor_rollout_ref.actor.optim.lr=3e-5 \
     actor_rollout_ref.actor.loss_agg_mode=seq-mean-token-sum-norm \
-    actor_rollout_ref.model.use_remove_padding=True \
+    +actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=8 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=30000 \
