90e1b4c6c178f9c5697b703048cef95e8a3b8b09
listar2000 <lisd.star2015@outlook.com>
2025-11-21 23:11:22 -0600

    refactor trajectory visualization logging

--- rllm/utils/__init__.py
+++ rllm/utils/__init__.py
@@ -2,5 +2,6 @@
 
 from rllm.utils.compute_pass_at_k import compute_pass_at_k
 from rllm.utils.episode_logger import EpisodeLogger
+from rllm.utils.visualization import VisualizationConfig, colorful_print, colorful_warning, visualize_trajectories
 
-__all__ = ["EpisodeLogger", "compute_pass_at_k"]
+__all__ = ["EpisodeLogger", "compute_pass_at_k", "visualize_trajectories", "VisualizationConfig", "colorful_print", "colorful_warning"]
