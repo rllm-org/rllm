4221088505f64372f1d0fb5f5ae92c12c3559ac8
xujunjielong.1 <xujunjielong.1@bytedance.com>
2026-02-16 15:21:10 +0100

    update:scripts

--- setup-and-debug.sh
+++ setup-and-debug.sh
@@ -14,13 +14,11 @@ if [ "$use_byted_venv" = true ]; then
     export HTTP_PROXY=http://sys-proxy-rd-relay.byted.org:8118
     export http_proxy=http://sys-proxy-rd-relay.byted.org:8118
     export https_proxy=http://sys-proxy-rd-relay.byted.org:8118
-    export no_proxy="localhost,.byted.org,byted.org,.bytedance.net,bytedance.net,.byteintl.net,.tiktok-row.net,.tiktok-row.org,127.0.0.1,127.0.0.0/8,169.254.0.0/16,100.64.0.0/10,172.16.0.0/12,192.168.0.0/16,10.0.0.0/8,::1,fe80::/10,fd00::/8"
 fi
 
 # uv venv setup (rllm+r2egym)
 [ -d ".venv" ] || uv venv --python 3.11
 source .venv/bin/activate
-echo "=== installing rLLM w./ VeRL ==="
 uv pip install -e ".[verl]"
 # if use byted cluster, install bytedray and byted-wandb
 if [ "$use_byted_venv" = true ]; then
